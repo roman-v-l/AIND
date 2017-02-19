fe759655d27c98a18fc985c2a4562f9456637a4c
roman-v-l <rlototskyy@gmail.com>
2017-02-19 12:49:22 -0500

    Updated the solution file

--- AIND-Sudoku/solution.py
+++ AIND-Sudoku/solution.py
@@ -188,7 +188,6 @@ def solve(grid):
     return search(grid_values(grid))
 
 if __name__ == '__main__':
-    print(diagonal_units)
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
     display(solve(diag_sudoku_grid))
 
